a32e1282ca32df64965ecfd4d92996e824897b69
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-01-14 23:13:36 +0900

    update tests

--- Tests/KeyPathValueTests/KeyPathValueApplierTests.swift
+++ Tests/KeyPathValueTests/KeyPathValueApplierTests.swift
@@ -21,27 +21,52 @@ final class KeyPathValueApplierTests: XCTestCase {
 
     func testWritableKeyPathValueApplier() {
         var applier: WritableKeyPathValueApplier<StructItem>
+        var isSucceeded: Bool
 
         applier = .init(\.number)
-        applier.apply(123, to: &structItem)
+        isSucceeded = applier.apply(123, to: &structItem)
+        XCTAssertTrue(isSucceeded)
 
         applier = .init(\.object)
-        applier.apply(Object(string: "123"), to: &structItem)
+        isSucceeded = applier.apply(Object(string: "123"), to: &structItem)
+        XCTAssertTrue(isSucceeded)
 
         XCTAssertEqual(structItem.number, 123)
         XCTAssertEqual(structItem.object, .init(string: "123"))
+
+        // fails
+        applier = .init(\.number)
+        isSucceeded = applier.apply("123", to: &structItem)
+        XCTAssertFalse(isSucceeded)
+
+        applier = .init(\.object)
+        isSucceeded = applier.apply("123", to: &structItem)
+        XCTAssertFalse(isSucceeded)
     }
 
     func testReferenceWritableKeyPathValueApplier() {
         var applier: ReferenceWritableKeyPathValueApplier<ClassItem>
+        var isSucceeded: Bool
 
         applier = .init(\.number)
-        applier.apply(123, to: classItem)
+        isSucceeded = applier.apply(123, to: classItem)
+        XCTAssertTrue(isSucceeded)
 
         applier = .init(\.object)
-        applier.apply(Object(string: "123"), to: classItem)
+        isSucceeded = applier.apply(Object(string: "123"), to: classItem)
+        XCTAssertTrue(isSucceeded)
 
         XCTAssertEqual(classItem.number, 123)
         XCTAssertEqual(classItem.object, .init(string: "123"))
+
+
+        // fails
+        applier = .init(\.number)
+        isSucceeded = applier.apply("123", to: classItem)
+        XCTAssertFalse(isSucceeded)
+
+        applier = .init(\.object)
+        isSucceeded = applier.apply("123", to: classItem)
+        XCTAssertFalse(isSucceeded)
     }
 }
